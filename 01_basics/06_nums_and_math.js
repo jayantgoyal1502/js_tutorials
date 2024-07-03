f0679908e61c25a50b11c2c80d63fe6e293e609b
jayantgoyal1502 <96365659+jayantgoyal1502@users.noreply.github.com>
2024-07-03 14:03:43 +0000

    Math function and numbers in js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+const val = new Number(100)
+// console.log(val);
+
+// console.log(val.toString());
+
+// console.log(val.toString().length);
+// console.log(val.toFixed(2));
+
+const anotherNumber = 234.674
+// console.log(anotherNumber.toPrecision(5));
+
+const num = 1000000
+// console.log(num.toLocaleString('en-IN'));
+
+// ******************************** Maths *************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.59));
+// console.log(Math.ceil(4.59));
+// console.log(Math.floor(4.59));
+// console.log(Math.min(4,5,3,7));
+// console.log(Math.max(4,5,3,7));
+
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() *  (max-min+1))+min)
