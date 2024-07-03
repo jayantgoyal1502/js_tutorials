615c0a69252b5339a125e89c674e828b53ccb56e
jayantgoyal1502 <96365659+jayantgoyal1502@users.noreply.github.com>
2024-07-03 12:52:19 +0000

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,41 @@ let someNumber  = 3
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************************** Operations ***********************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="hello"
+let str2 = "jayant"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
