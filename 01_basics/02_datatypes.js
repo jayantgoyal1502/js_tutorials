fb21cc5f9243de88b2977809257679335d9d7c6f
jayantgoyal1502 <96365659+jayantgoyal1502@users.noreply.github.com>
2024-07-03 12:30:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+5) // we are using nodejs , not browser
+
+console.log(3
+    +
+    3) // do not write code like this. readability should be high
+
+let name = "jayant"
+let age = 21
+
+
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+// symbol => unique
+
+
+// object
+
+console.log(typeof "jayant");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
