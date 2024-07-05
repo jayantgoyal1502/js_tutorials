18ba5a307ae20474fa48599806c06ccba353e770
jayantgoyal1502 <96365659+jayantgoyal1502@users.noreply.github.com>
2024-07-05 04:47:16 +0000

    scopes and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,46 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    console.log("Inner: ",a); //this is block scope
+    // console.log("Inner: ",a); //this is block scope
 }
 
-console.log(a); //a is not defined
+// console.log(a); //a is not defined
 // console.log(b); //b is not defined
 // console.log(c); //30 //that's why we don't use var
+
+
+function one(){
+    const username = "jayant"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(website);
+    // two()
+}
+// one()
+
+if(true){
+    const username = "jayant"
+    if(username === "jayant"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// console.log(addOne(5)) // it will run fine
+function addOne(num){
+    return num+1
+}
+
+
+// console.log(addTwo(5)) // this will throw error because excecuted above declaration in this syntax. this thing is called hoisting
+const addTwo = function(num){
+    return num+2
+}
