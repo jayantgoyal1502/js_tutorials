879ba94921a9d30a20432b500fc4366899f3053e
jayantgoyal1502 <96365659+jayantgoyal1502@users.noreply.github.com>
2024-07-05 02:50:09 +0000

    objects in javascript

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,40 @@
+// const tinderUser = new Object() //singleton object
+const tinderUser = {} //non singleton object
+tinderUser.id = "123abc"
+tinderUser.name = "tony stark"
+// console.log(tinderUser);
+
+const user2 = {
+    email: "name@google.com",
+    fullName : {
+        username : {
+            firstname: "tony",
+            lastname : "stark"
+        }
+    }
+}
+
+// console.log(user2.fullName.username.lastname);
+
+const obj1 = {
+    1: "a",
+    2:"b"
+}
+const obj2 = {
+    3:"c",
+    4:"d"
+}
+
+// const obj3 = {obj1,obj2}
+// console.log(obj3);
+
+// const obj3 = Object.assign({},obj1,obj2) //{} it is added optionally but is a gud practice
+// console.log(obj3);
+
+// const obj3 = {...obj1, ...obj2} //most used method , spread
+// console.log(obj3);
+
+
+console.log(Object.keys(tinderUser)); // [ 'id', 'name' ]
+console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'tony stark' ] ]
+console.log(Object.values(tinderUser)); // [ '123abc', 'tony stark' ]
