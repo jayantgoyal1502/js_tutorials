e59f82c1ca1f016ed35e9551e339480f212e2742
jayantgoyal1502 <96365659+jayantgoyal1502@users.noreply.github.com>
2024-07-16 16:33:50 +0000

    object properties

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,28 @@
+const descriptor  = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descriptor);
+
+const chai = {
+    name : "ginger chai",
+    price : 250,
+    isAvailable : true,
+
+    orderChai : function(){
+        console.log("Chai nahi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+// Object.defineProperty(chai, "name" , {
+//     writable: false,
+//     enumerable: false
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key}:${value}`);
+    }
+}
